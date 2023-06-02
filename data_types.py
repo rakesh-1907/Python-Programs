d2a8198d54d540ea960c52d454bae8ec9a79810a
rakesh-1907 <patil.rakesh1907@gmail.com>
2023-06-02 20:03:51 +0530

    print function added in code

--- data_types.py
+++ data_types.py
@@ -1,5 +1,9 @@
 list=[10,20,30,40,50]
+print(list)
 tuples=(60,70,'rakesh','shaurya')
+print(tuples)
 set={1,2,3,'sharvi','rohan','pavan'}
+print(set)
 dict={1:'java',2:'pythin',3:'c++',4:'c'}
+print(dict)
 
