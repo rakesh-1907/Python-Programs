2ba3dd6ce4716dc69da93f2d586a06562b33d44c
Rakesh <patil.rakesh1907@gmail.com>
2023-06-06 18:00:40 +0530

    while code

---
+++ while.py
@@ -0,0 +1,30 @@
+#i=1 #initialization
+
+#while i<=5: # Condition
+#    print("Python")
+
+#    i=i+1 # increment/decrement
+
+
+
+#i=5
+
+#while i>=1:
+#    print("Python",i)
+
+#    i=i-1
+
+
+
+i=1
+
+while i<=5:
+    print("Python Is",end=" ")
+
+    j=1
+    while j<=4:
+        print("Awsome",end=" ")
+        j=j+1
+
+    i=i+1
+    print()
