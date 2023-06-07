ccb0f1bc392f23277f2ce0d34b030345aa809448
Rakesh <patil.rakesh1907@gmail.com>
2023-06-07 16:22:06 +0530

    file modified

--- assignment.py
+++ assignment.py
@@ -6,9 +6,27 @@
 
 
 
-for i in range(1,101):
-    if i%3==0 or i%5==0:
-        continue
-    print(i)
+#for i in range(1,101):
+#    if i%3==0 or i%5==0:
+#        continue
+#    print(i)
 
-print("Bye")
+#print("Bye")
+
+
+#for i in range(5):
+#    if i==3:
+#        break
+#    print("hello",i)
+
+
+
+#for i in range(5):
+#   if i==3:
+#        continue
+#    print("hello",i)
+
+
+#for i in range(1,101):
+#    if i%2==0:
+#     print(i)
