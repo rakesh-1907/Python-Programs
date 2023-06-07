2916ee4a7332d12f06c0f26d3d2015f001f06271
Rakesh <patil.rakesh1907@gmail.com>
2023-06-07 11:22:37 +0530

    Programs using for loop

---
+++ for.py
@@ -0,0 +1,27 @@
+#x = ['Rakesh',2.5,100]
+
+#for i in x:
+#    print(i)
+
+
+
+
+#for i in range(1,10):
+#    print(i)
+
+
+#for i in range(11,21,2):
+#    print(i)
+
+
+
+#for i in range(11,0,-1):
+#    print(i)
+
+
+for i in range(1,21):
+    if(i%5!=0):
+        print(i)
+
+
+
