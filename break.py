0f020c56351df520d1cdaef6d1d446a4635bf7ff
Rakesh <patil.rakesh1907@gmail.com>
2023-06-07 14:54:05 +0530

    programe using break treatment

---
+++ break.py
@@ -0,0 +1,15 @@
+x=int(input("Enter the how many candy's you want "))
+
+av=5
+
+i=1
+while i<=x:
+
+    if i>av:
+        print("We are out of stock")
+        break
+
+    print("Candys")
+    i+=1
+
+print("bye")
