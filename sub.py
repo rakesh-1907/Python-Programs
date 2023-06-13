53e645b23be3d5bb7061ce2f0e046da85bea55dd
Rakesh <patil.rakesh1907@gmail.com>
2023-06-13 16:48:18 +0530

    multiplication code

--- sub.py
+++ sub.py
@@ -1,6 +1,10 @@
+
+
 value1 = int(input("Enter the first value "))
 value2 = int(input("Enter the second value "))
 
-result = value1-value2
+result = value1*value2
 
 print(result)
+
+#updated code for multiplication
